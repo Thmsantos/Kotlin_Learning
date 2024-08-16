bdab259b3f3801c327e61ee3f59b3714d41351f2
Thiago Santos <91476713+Thmsantos@users.noreply.github.com>
2024-08-16 08:38:10 -0300

    Exercício formulário 16.08 - PPDM

--- Multiplicacao.kt
+++ Multiplicacao.kt
@@ -1,3 +1,8 @@
+//FATEC LUIGI PAPAIZ
+//THIAGO MESSIAS SANTOS
+//DIADEMA, SP.
+//16.08
+
 fun main() {
     var a: Int = readLine().toString().toInt();
     var b: Int = readLine().toString().toInt();
