462c6bd8b6a405ada3893b59768d794708d9ef23
Thiago Santos <91476713+Thmsantos@users.noreply.github.com>
2024-08-16 08:36:39 -0300

    Exercícios formulário 16.08 - PPDM

--- CelsiusForFahrenheit.kt
+++ CelsiusForFahrenheit.kt
@@ -1,3 +1,8 @@
+//FATEC LUIGI PAPAIZ
+//THIAGO MESSIAS SANTOS
+//DIADEMA, SP.
+//16.08
+
 fun main(){
   val c: Double = readLine().toString().toDouble();
   print("Fahrenheit: ${((9 * c) + 160) / 5}");
